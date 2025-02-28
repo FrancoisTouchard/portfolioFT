7ed527662a6887af3f9563d73ab307dd1ac9b722
Francois Touchard <francois.touchard.wos@gmail.com>
2025-02-28 12:21:25 +0100

    build(config): adding ts, eslint and prettier config

--- portfolioFT/eslint.config.js
+++ portfolioFT/eslint.config.js
@@ -3,9 +3,13 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import prettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ["node_modules", 'dist'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -17,11 +21,32 @@ export default [
         sourceType: 'module',
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: { react: { version: 'detect'} },
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
     plugins: {
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort' : simpleImportSort,
+      'prettier': prettier,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+      'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+      'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
+    parserOptions: {
+      ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+      sourceType: 'module', // Allows for the use of imports
+      ecmaFeatures: {
+        jsx: true, // Allows for the parsing of JSX
+      },
+    },
+    env: {
+      browser: true,
+      node: true,
+      es6: true,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -33,6 +58,39 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          ['^\\u0000'], // side effect imports
+          ['^@?\\w'], // packages
+          ['^[^.]'], // everything else
+          ['^\\.'], // relative imports
+        ],
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { vars: 'all', varsIgnorePattern: '^_$', args: 'after-used', ignoreRestSiblings: false },
+    ],
+    '@typescript-eslint/no-shadow': ['error'],
+    "prettier/prettier": "error", // Enforce Prettier formatting
+    indent: ["error", 2], // Ensure 2-space indentation
     },
   },
+  eslintConfigPrettier
 ]
