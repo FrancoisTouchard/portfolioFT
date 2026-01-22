068148a365e85437fc31ba7e8993a0022727a35e
François Touchard <francois@m5ft.home>
2026-01-22 19:37:42 +0100

    feat(Introduction): creating Intro component

---
+++ src/components/Introduction/component.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Introduction.css'
+import profilePic from '../../assets/yuji.jpg';
+
+
+export default function Introduction() {
+  return (
+    <div className="squares-container">
+      <div className="square">
+        <div className="inner-square">
+          <img className='profilepic' src={profilePic} alt={"Photo de moi"} />
+        </div>
+      </div >
+      <div className="square">
+        <div className="inner-square">
+          <span className='text-job'>Développeur front-end</span>
+        </div>
+      </div >
+    </div>
+  );
+}
--- src/screens/Home/component.tsx
+++ src/screens/Home/component.tsx
@@ -3,14 +3,18 @@ import { useNavigate } from "react-router-dom";
 import './Home.css'
 import PageView from "../../components/PageView/component";
 import Timeline from "../../components/Timeline/component";
+import Introduction from "../../components/Introduction/component";
 
 const Home = () => {
   const navigate = useNavigate();
   return (
     <PageView>
-      <div className="HomeContainer">
-        <h1>Home Page</h1>
-        <button onClick={() => navigate("/skills")}>Go to skills</button>
+      <div className="home-container">
+        <div className="intro-container">
+          <Introduction />
+        </div>
+        {/* <h1>Home Page</h1>
+        <button onClick={() => navigate("/skills")}>Go to skills</button> */}
         <Timeline />
       </div>
     </PageView>
