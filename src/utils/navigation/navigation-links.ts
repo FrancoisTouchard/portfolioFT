1530d3fa5398f81ba4b58aa5f14d3586f89fb1c4
François Touchard <francois@m5ft.home>
2026-03-18 17:55:25 +0100

    feat(Navigation): adding navlinks object

---
+++ src/utils/navigation/navigation-links.ts
@@ -0,0 +1,20 @@
+export const SKILLS_NAV_LINKS = [
+  { path: 'javascript', label: 'JavaScript' },
+  { path: 'typescript', label: 'TypeScript' },
+  { path: 'react-native', label: 'React Native' },
+  { path: 'react', label: 'React' },
+  { path: 'uxui', label: 'UX/UI' },
+  { path: 'communication', label: 'Communication' },
+  { path: 'creativity', label: 'Créativité' },
+  { path: 'critical-thinking', label: 'Esprit Critique' },
+  { path: 'commitment', label: 'Implication' },
+  { path: 'organization', label: 'Organisation' },
+];
+
+export const WORKS_NAV_LINKS = [
+  { path: 'multimedia-carrousel', label: 'Carrousel Multimédia' },
+  { path: 'wiki', label: 'Cours & Wiki' },
+  { path: 'indigo-project', label: 'The Indigo Project' },
+  { path: 'mygiflibrary', label: 'My Gif Library' },
+  { path: 'chatbot', label: 'Pickme & HelloMyBot' },
+];
