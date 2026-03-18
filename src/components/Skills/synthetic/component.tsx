471d5c87ebf246ba0bc5d613f2c83afa414c9af7
François Touchard <francois@m5ft.home>
2026-03-18 18:21:11 +0100

    refactor(Skills,-Works): uniformizing font sizes and spacings

--- src/components/Skills/synthetic/component.tsx
+++ src/components/Skills/synthetic/component.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
 import './Skills.css';
+
+import React from 'react';
+
 import { SKILLS } from '../skills.data';
 import SkillCard from './card/component';
 
 const Skills = () => {
-
-  const hard_skills = SKILLS.filter(s => s.category === "HARD")
-  const soft_skills = SKILLS.filter(s => s.category === "SOFT")
+  const hard_skills = SKILLS.filter(s => s.category === 'HARD');
+  const soft_skills = SKILLS.filter(s => s.category === 'SOFT');
 
   return (
-    <div className='skills-page-container'>
+    <div className="skills-page-container">
       <h1 className="skills-page-title">Mes compétences</h1>
       <h2 className="skills-subtitle">Techniques</h2>
       <div className="skills-cards-container">
-        {hard_skills.map((skill) => {
+        {hard_skills.map(skill => {
           return (
             <SkillCard
+              key={skill.title}
               title={skill.title}
               logo={skill.logo}
               slug={skill.slug}
@@ -25,9 +27,10 @@ const Skills = () => {
       </div>
       <h2 className="skills-subtitle">Humaines</h2>
       <div className="skills-cards-container">
-        {soft_skills.map((skill) => {
+        {soft_skills.map(skill => {
           return (
             <SkillCard
+              key={skill.title}
               title={skill.title}
               logo={skill.logo}
               slug={skill.slug}
--- src/components/Works/component.tsx
+++ src/components/Works/component.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-
 import './Works.css';
-import { WORKS } from './works.data';
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 import WorksCard from './synthetic/component';
+import { WORKS } from './works.data';
 
 const Works = () => {
-
   return (
-    <div className='works-page-container'>
+    <div className="works-page-container">
       <h1 className="works-page-title">Mes réalisations</h1>
-      <div className='works-cards-container'>
-        {WORKS.map((work) => {
+      <div className="works-cards-container">
+        {WORKS.map(work => {
           return (
-            <div
-              key={work.id}
-              className="works-card-item"
-            >
+            <div key={work.id} className="works-card-item">
               <Link className="works-card-link" to={`/works/${work.slug}`}>
                 <WorksCard
                   description={work.description}
