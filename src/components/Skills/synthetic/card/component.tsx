8ee0adb144250b279fc1ab34cd563183c60a9336
François Touchard <francois@M5FT.local>
2026-04-03 17:57:54 +0200

    refactor(SkillCard): card width and spacing on mobile

--- src/components/Skills/synthetic/card/component.tsx
+++ src/components/Skills/synthetic/card/component.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-
 import './SkillCard.css';
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 import { SkillsCardProps } from './types';
 
 const SkillCard = ({ title, logo, slug }: SkillsCardProps) => {
   return (
     <Link className="skills-card" to={`/skills/${slug}`}>
-      <img className='skills-card-logo' src={logo.src} alt={logo.alt} />
+      <img className="skills-card-logo" src={logo.src} alt={logo.alt} />
       <h3 className="skills-card-title">{title}</h3>
     </Link>
   );
