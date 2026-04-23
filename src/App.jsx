34df5530d77e55ab3565980b824dda2e52731c25
francois <francois.touchard.wos@gmail.com>
2026-04-23 14:14:29 +0200

    perf(Routes): adding dynamic import on routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import './App.css';
 
+import { lazy, Suspense } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Footer from './components/Footer/component';
 import NavBar from './components/NavBar/component';
 import ScrollToTop from './components/ScrollToTop/component';
-import About from './screens/About/component';
-import Contact from './screens/Contact/component';
-import ExperienceDetailsPage from './screens/Experience/component';
-import Home from './screens/Home/component';
-import SkillsDetailsPage from './screens/Skills/details/component';
-import SkillsPage from './screens/Skills/synthetic/component';
-import WorkDetailsPage from './screens/Work/details/component';
-import Works from './screens/Work/synthetic/component';
+
+const About = lazy(() => import('./screens/About/component'));
+const Contact = lazy(() => import('./screens/Contact/component'));
+const ExperienceDetailsPage = lazy(
+  () => import('./screens/Experience/component'),
+);
+const Home = lazy(() => import('./screens/Home/component'));
+const SkillsDetailsPage = lazy(
+  () => import('./screens/Skills/details/component'),
+);
+const SkillsPage = lazy(() => import('./screens/Skills/synthetic/component'));
+const WorkDetailsPage = lazy(() => import('./screens/Work/details/component'));
+const Works = lazy(() => import('./screens/Work/synthetic/component'));
 
 function App() {
   return (
@@ -22,22 +28,24 @@ function App() {
         <div className="app-layout">
           <NavBar />
           <main className="app-content">
-            <Routes>
-              <Route index element={<Home />} />
-              <Route
-                path="/experiences/:experienceSlug"
-                element={<ExperienceDetailsPage />}
-              />
-              <Route path="/skills" element={<SkillsPage />} />
-              <Route
-                path="/skills/:skillSlug"
-                element={<SkillsDetailsPage />}
-              />
-              <Route path="/about" element={<About />} />
-              <Route path="/works" element={<Works />} />
-              <Route path="/works/:workSlug" element={<WorkDetailsPage />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
+            <Suspense fallback={null}>
+              <Routes>
+                <Route index element={<Home />} />
+                <Route
+                  path="/experiences/:experienceSlug"
+                  element={<ExperienceDetailsPage />}
+                />
+                <Route path="/skills" element={<SkillsPage />} />
+                <Route
+                  path="/skills/:skillSlug"
+                  element={<SkillsDetailsPage />}
+                />
+                <Route path="/about" element={<About />} />
+                <Route path="/works" element={<Works />} />
+                <Route path="/works/:workSlug" element={<WorkDetailsPage />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+            </Suspense>
           </main>
           <Footer />
         </div>
